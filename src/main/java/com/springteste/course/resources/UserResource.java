6c9855e1b163e1ef5fea5d2d532998460f5ca812
RamonFranco <ramonccastro@hotmail.com>
2023-03-18 11:51:37 -0300

    Criação da classe User e User

---
+++ src/main/java/com/springteste/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.springteste.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springteste.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "maria","maria@gmail.com", "0000000000", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
