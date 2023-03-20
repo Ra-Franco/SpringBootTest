c327148fcd556f14720e9cf5c25a9204c6a0732d
RamonFranco <ramonccastro@hotmail.com>
2023-03-20 00:14:44 -0300

    Criação da entidade Order, Service Order, Repository Order e Resource Order

---
+++ src/main/java/com/springteste/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.springteste.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springteste.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
