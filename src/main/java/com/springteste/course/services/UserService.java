4ce38e3f88414908bebb946ad7b2450ed7a3e9bd
RamonFranco <ramonccastro@hotmail.com>
2023-03-22 20:44:22 -0300

    Criação da exception para o metodo update, EntityNotFoundException

--- src/main/java/com/springteste/course/services/UserService.java
+++ src/main/java/com/springteste/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.springteste.course.repositories.UserRepository;
 import com.springteste.course.services.exceptions.DatabaseException;
 import com.springteste.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -42,9 +44,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	public void updateData(User entity, User obj) {
