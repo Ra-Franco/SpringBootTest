8a5421f6711a0e2b84819af33b69e64bc2f7a380
RamonFranco <ramonccastro@hotmail.com>
2023-03-22 20:40:12 -0300

    Criação de Exception para o metodo delete

--- src/main/java/com/springteste/course/services/UserService.java
+++ src/main/java/com/springteste/course/services/UserService.java
@@ -3,10 +3,13 @@ package com.springteste.course.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.springteste.course.entities.User;
 import com.springteste.course.repositories.UserRepository;
+import com.springteste.course.services.exceptions.DatabaseException;
 import com.springteste.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,7 +32,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
